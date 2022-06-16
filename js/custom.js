a0bd7899b4cf30967696035a4be285311b4ae75a
jina0507 <107232356+jina0507@users.noreply.github.com>
2022-06-16 13:39:19 +0900

    1.2

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,21 @@
 
 $(function(){
+  $(window).scroll(function(){//window객체에 스크롤 이벤트 발생
+    var scroll = $(window).scrollTop();//변수 scroll에 현재 스크롤한 만큼의 거리를 저장
+    console.log(scroll);
+    if(scroll > 20){//스크롤이동값이 20보다 클때 구문실행
+      $(".fixbtn").css("display","block");//버튼요소를 화면에 표시
+    }else{//스크롤이동값이 20보다 작을때 구문실행
+      $(".fixbtn").css("display","none");//버튼요소를 화면에서 제거
+    }
+  });
+
+  //top버튼 클릭시
+  $(".fixbtn").click(function(){
+    $(window).scrollTop(0);//window객체의 스크롤위치값을 0(페이지상단)으로 설정
+  })
+
+  
   $(window).scroll(function(){
     var scroll = $(window).scrollTop();//스크롤 이동값을 변수scroll에 할당
     var pakageTop = $(".pakage").offset().top-200;//worksTop변수에 .works의 top위치값을 저장
